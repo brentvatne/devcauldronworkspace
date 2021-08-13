341e19b0b052ec1a61ce5a3a6e56d423d5b79c96
Brent Vatne <brentvatne@gmail.com>
2021-08-13 14:27:06 -0700

    back

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import MapView from 'react-native-maps';
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 import { MyView } from "expo-custom";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello from the application</Text>
+      <MapView style={styles.map} />
       <MyView />
       <StatusBar style="auto" />
     </View>
@@ -19,5 +21,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
