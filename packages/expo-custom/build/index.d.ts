243bb619f75f9d38231a5d8a3ac5aae9e8a745ab
Brent Vatne <brentvatne@gmail.com>
2021-08-13 13:53:24 -0700

    Initial commit
    Generated by create-react-native-app 3.6.0.

---
+++ packages/expo-custom/build/index.d.ts
@@ -0,0 +1,1 @@
+export declare function MyView(): JSX.Element;
---
+++ packages/expo-custom/build/index.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+// @ts-ignore
+import { Text } from 'react-native';
+export function MyView() {
+    return React.createElement(Text, null, "Hello from the first package");
+}
+//# sourceMappingURL=index.js.map
